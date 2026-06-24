0780fbc2985b85032b008da8a2a38f02322b6d37
tde-alme <tatiana-esteves@sapo.pt>
2026-06-24 13:24:33 +0000

    2406

--- swap.c
+++ swap.c
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap_utils.c                                  :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tde-alme <tde-alme@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 14:37:13 by tde-alme          #+#    #+#             */
-/*   Updated: 2026/06/17 17:11:00 by tde-alme         ###   ########.fr       */
+/*   Updated: 2026/06/24 13:22:47 by tde-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	sa(int *stack_a, int *size_a) //swap top 2 a
+void	sa(t_node **stack_a) //swap top 2 a
 {
 	int	temp;
 
-	if (*size_a < 2)
+	if ((!*stack_a) || (!*stack_a -> next))
 		return ;
-	temp = stack_a[0];
-	stack_a[0] = stack_a[1];
-	stack_a[1] = temp;
+	temp = (*stack_a) -> value;
+	(*stack_a) -> value= (*stack_a) -> next -> value;
+	(*stack_a) -> next -> value = temp;
 	write(1, "sa\n", 3);
 }
 
-void	sb(int *stack_b, int *size_b) //swap top 2 b
+void	sb(t_node **stack_b) //swap top 2 b
 {
 	int	temp;
 
-	if (*size_b < 2)
+	if ((!*stack_b) || (!*stack_b -> next))
 		return ;
-	temp = stack_b[0];
-	stack_b[0] = stack_b[1];
-	stack_b[1] = temp;
+	temp = (*stack_b) -> value;
+	(*stack_b) -> value= (*stack_b) -> next -> value;
+	(*stack_b) -> next -> value = temp;
 	write(1, "sb\n", 3);
 }
 
-void	ss(int *stack_a, int *stack_b, int *size_a, int *size_b) //swap top 2 a & b
+void	ss(t_node **stack_a, t_node **stack_b) //swap top 2 a & b
 {
-	sa(stack_a, size_a);
-	sb(stack_b, size_b);
+	sa(stack_a);
+	sb(stack_b);
 }
