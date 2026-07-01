fcf35b1752d1d5bb9424f4984127808daebff1d7
josmorei <josmorei@student.42porto.com>
2026-07-01 16:40:02 +0100

    01/07/2026

--- sort_two_or_three.c
+++ sort_two_or_three.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_two_or_three.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tde-alme <tde-alm@student.42porto.com>     +#+  +:+       +#+        */
+/*   By: josmorei <josmorei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 14:31:36 by tde-alme          #+#    #+#             */
-/*   Updated: 2026/06/26 16:28:23 by tde-alme         ###   ########.fr       */
+/*   Updated: 2026/07/01 15:53:48 by josmorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,37 @@
 
 void	sort_two(t_node **stack_a)
 {
-	if((*stack_a)-> value > (*stack_a)-> next-> value)
+	if ((*stack_a)->value > (*stack_a)->next->value)
 		sa(stack_a);
 }
 
 void	sort_three(t_node **stack_a)
 {
-    t_node	*sec;
-    t_node	*third;
-    
-    sec = (*stack_a)-> next;
-	third = sec-> next;
-    if(((*stack_a)-> value < sec-> value) && (sec-> value < third-> value))
-		return;
-	else if(((*stack_a)-> value > third-> value) && (sec-> value > (*stack_a)-> value) && (third-> value < sec-> value))
+	t_node	*sec;
+	t_node	*third;
+
+	sec = (*stack_a)->next;
+	third = (*stack_a)->prev;
+	if (((*stack_a)->value < sec->value) && (sec->value < third->value))
+		return ;
+	else if (((*stack_a)->value > third->value)
+		&& (sec->value > (*stack_a)->value) && (third->value < sec->value))
 		rra(stack_a);
-	else if(((*stack_a)-> value < sec-> value) && (sec-> value > third-> value) && ((*stack_a)-> value < third-> value))
-	{	
+	else if (((*stack_a)->value < sec->value) && (sec->value > third->value)
+		&& ((*stack_a)->value < third->value))
+	{
 		sa(stack_a);
 		ra(stack_a);
 	}
-	else if(((*stack_a)-> value > sec-> value) && (sec-> value < third-> value) && ((*stack_a)-> value < third-> value))
+	else if (((*stack_a)->value > sec->value) && (sec->value < third->value)
+		&& ((*stack_a)->value < third->value))
 		sa(stack_a);
-	else if(((*stack_a)-> value > sec-> value) && (sec-> value < third-> value) && ((*stack_a)-> value > third-> value))
+	else if (((*stack_a)->value > sec->value) && (sec->value < third->value)
+		&& ((*stack_a)->value > third->value))
 		ra(stack_a);
-	else if(((*stack_a)-> value > sec-> value) && (sec-> value > third-> value) && ((*stack_a)-> value > third-> value))
-	{	
+	else if (((*stack_a)->value > sec->value) && (sec->value > third->value)
+		&& ((*stack_a)->value > third->value))
+	{
 		sa(stack_a);
 		rra(stack_a);
 	}
--- stack_size.c
+++ stack_size.c
@@ -6,20 +6,25 @@
 /*   By: josmorei <josmorei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 16:42:42 by tde-alme          #+#    #+#             */
-/*   Updated: 2026/06/27 16:28:31 by josmorei         ###   ########.fr       */
+/*   Updated: 2026/07/01 13:43:46 by josmorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int stack_size(t_node **stack_a)
+int	stack_size(t_node **stack_a)
 {
-    t_node  node;
-    int     count;
+	t_node	*node;
+	int		count;
 
-    node = *stack_a;
-    count = 0;
-    while(node-> next)
-        count = count + node-> next;   
-    return (count);        
+	if (!*stack_a)
+		return (0);
+	node = *stack_a;
+	count = 1;
+	while (node->next != *stack_a)
+	{
+		node = node->next;
+		count++;
+	}
+	return (count);
 }
