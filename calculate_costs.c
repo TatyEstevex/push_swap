7c052e76112fd030ac11d3f553773401293c8bd5
tde-alme <tatiana-esteves@sapo.pt>
2026-07-07 17:38:34 +0100

    0707.17h30

--- calculate_costs.c
+++ calculate_costs.c
@@ -6,7 +6,7 @@
 /*   By: tde-alme <tde-alme@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 11:40:32 by tde-alme          #+#    #+#             */
-/*   Updated: 2026/07/02 13:37:52 by tde-alme         ###   ########.fr       */
+/*   Updated: 2026/07/07 16:19:13 by tde-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,22 @@ int	cost_b(int pos_b, int size_b)
 	return (size_b - pos_b);
 }
 
+static int	get_total_cost(int pos_a, int pos_b, int size_a, int size_b)
+{
+	int	ca;
+	int	cb;
+
+	ca = cost_a(pos_a, size_a);
+	cb = cost_b(pos_b, size_b);
+	if ((pos_a <= size_a / 2 && pos_b <= size_b / 2)
+		|| (pos_a > size_a / 2 && pos_b > size_b / 2))
+	{
+		if (ca > cb)
+			return (ca);
+		return (cb);
+	}
+	return (ca + cb);
+}
 void	calculate_costs(t_node **stack_a, t_node **stack_b)
 {
 	int		size_a;
@@ -41,8 +57,9 @@ void	calculate_costs(t_node **stack_a, t_node **stack_b)
 	current = *stack_b;
 	do
 	{
-		current->cost = cost_b(pos_b, size_b)
-			+ cost_a(find_position(stack_a, current->value), size_a);
+		current->cost = get_total_cost(
+		find_position(stack_a, current->value),
+		pos_b, size_a, size_b);
 		current = current->next;
 		pos_b++;
 	} while (current != *stack_b);
--- swap_cheapest.c
+++ swap_cheapest.c
@@ -6,7 +6,7 @@
 /*   By: tde-alme <tde-alme@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 13:59:30 by tde-alme          #+#    #+#             */
-/*   Updated: 2026/07/02 11:29:05 by tde-alme         ###   ########.fr       */
+/*   Updated: 2026/07/07 15:50:46 by tde-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,11 +54,42 @@ void    rotate_a(t_node **stack_a, int pos, int size_a)
 	}
 }
 
+void	rotate_both(t_node **stack_a, t_node **stack_b, int pos_a, int pos_b,
+		int size_a, int size_b)
+{
+	while (pos_a > 0 && pos_b > 0)
+	{
+    	rr(stack_a, stack_b);
+    	pos_a--;
+    	pos_b--;
+	}
+	rotate_a(stack_a, pos_a, size_a);
+	rotate_b(stack_b, pos_b, size_b);
+}
+
+void	reverse_rotate_both(t_node **stack_a, t_node **stack_b, int pos_a, int pos_b,
+		int size_a, int size_b)
+{
+	pos_a = size_a - pos_a;
+	pos_b = size_b - pos_b;
+
+	while (pos_a > 0 && pos_b > 0)
+	{
+    	rrr(stack_a, stack_b);
+    	pos_a--;
+    	pos_b--;
+	}
+	while (pos_a-- > 0)
+    	rra(stack_a);
+	while (pos_b-- > 0)
+    	rrb(stack_b);
+}
+
 void	swap_cheapest(t_node **stack_a, t_node **stack_b)
 {
 	t_node *change;
 	t_node *current;
-	int		pos;
+	int		pos_b;
 	int		size_b;
     int     pos_a;
     int     size_a;
@@ -67,15 +98,23 @@ void	swap_cheapest(t_node **stack_a, t_node **stack_b)
     size_b = stack_size(stack_b);
 	change = find_cheapest(stack_b);
 	current = *stack_b;
-	pos = 0;
+	pos_b = 0;
 	while(current != change)
 	{
 		current = current -> next;
-		pos++;
+		pos_b++;
 	}
     pos_a = find_position(stack_a, change->value);
 	printf("inserir %d na posicao %d\n", change->value, pos_a);
-    rotate_b(stack_b, pos, size_b);
-    rotate_a(stack_a, pos_a, size_a);
+	if (pos_a <= size_a / 2 && pos_b <= size_b / 2)
+		rotate_both(stack_a, stack_b, pos_a, pos_b, size_a, size_b);
+	else if (pos_a > size_a / 2 && pos_b > size_b / 2)
+		reverse_rotate_both(stack_a, stack_b, pos_a, pos_b,
+			size_a, size_b);
+	else
+	{
+		rotate_a(stack_a, pos_a, size_a);
+		rotate_b(stack_b, pos_b, size_b);
+	}
     pa(stack_a, stack_b);
-}
+} 
